17627a31dcac97edff0aece06df41bc595a77d88
Alen Budimir <7889389+budi1200@users.noreply.github.com>
2021-12-07 21:18:48 +0100

    day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,48 @@
-pub fn day1() {
-    println!("TODO: Put day 1 code here")
+use std::fs;
+
+pub fn day1_1() {
+    let measurements_string = fs::read_to_string("./data/day1.txt").expect("Failed to read file");
+    let mut measurements = measurements_string.lines();
+
+    let mut increases = 0;
+    let mut prev: i32 = measurements.next().unwrap().parse().unwrap();
+
+    for line in measurements {
+        let tmp = line.parse().unwrap();
+
+        if tmp > prev {
+            increases += 1;
+        }
+
+        prev = tmp;
+    }
+
+    println!("{}", increases);
+}
+
+pub fn day1_2() {
+    let measurements_string = fs::read_to_string("./data/day1.txt").expect("Failed to read file");
+    let measurements: Vec<&str> = measurements_string.lines().collect();
+    let mes_len = measurements.len();
+
+    let mut increases = -1;
+    let mut last_sum = 0;
+
+    for n in 0..mes_len {
+        if n + 2 >= mes_len {
+            break;
+        }
+
+        let tmp = measurements[n].parse::<i32>().unwrap()
+            + measurements[n + 1].parse::<i32>().unwrap()
+            + measurements[n + 2].parse::<i32>().unwrap();
+
+        if tmp > last_sum {
+            increases += 1;
+        }
+
+        last_sum = tmp;
+    }
+
+    println!("{}", increases);
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::collections::HashMap;
 use std::env;
 
 fn main() {
+    // Usage: cargo run <day>.<part>
+    // Example: cargo run 1.2
     // Pass a day number as first argument
     let args: Vec<String> = env::args().collect();
 
@@ -11,17 +13,15 @@ fn main() {
         return println!("Invalid arguments provided");
     }
 
-    let day: i32 = match args[1].parse() {
-        Ok(day) => day,
-        Err(_) => return println!("Error parsing day!"),
-    };
+    let day = &args[1];
 
     println!("Chosen Day {}", day);
 
-    let mut available_days = HashMap::<i32, fn() -> ()>::new();
-    available_days.insert(1, day1::day1);
+    let mut available_days = HashMap::<&str, fn() -> ()>::new();
+    available_days.insert("1.1", day1::day1_1);
+    available_days.insert("1.2", day1::day1_2);
 
-    let day_to_run = available_days.get_key_value(&day);
+    let day_to_run = available_days.get_key_value(&day as &str);
 
     if day_to_run == None {
         return println!("Day not found!");
